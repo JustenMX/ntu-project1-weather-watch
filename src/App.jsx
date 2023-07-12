efcac6735ad0d99dff815d289033c0cd75d3f3da
nelson6726e <cheefong0903@gmail.com>
2023-07-12 21:26:22 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 // css
 import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
 // dependencies
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 // pages
 import LandingPage from "./pages/LandingPage";
 import Dashboard from "./pages/Dashboard";
@@ -21,21 +23,76 @@ import regionalData from "./data/regionalData";
 function App() {
   // state management
   const [region, setRegion] = useState(regionalData);
+  const [selectRegion, setSelectRegion] = useState("");
+  const [watchList, setWatchList] = useState([]);
+  const [isOptionSelected, setIsOptionSelected] = useState(false);
+
+  // handler Selected Option
+  const handlerSelectOption = (value) => {
+    if (value === "" || value === "Choose your location") {
+      setIsOptionSelected(false);
+      toast.warning("Please select a valid location");
+    } else {
+      setIsOptionSelected(true);
+      setSelectRegion(value);
+    }
+  };
+
+  //handler to Add WatchList
+  const handlerAddWatchList = () => {
+    // I do not need this but this adds as an additional safety barrier
+    // will decide later if i need to remove the validation for the onclick
+    if (selectRegion === "") {
+      setIsOptionSelected(false);
+      toast.error("Please select a valid location");
+    } else {
+      const newWatchList = {
+        region: selectRegion,
+      };
+      // Check if the same region is already in the watchlist
+      const checkDuplicates = watchList.some(
+        (watchItem) => watchItem.region === selectRegion
+      );
+      if (checkDuplicates) {
+        toast.error("You already have this location in your watch list");
+      } else {
+        setWatchList((prevState) => [...prevState, newWatchList]);
+        toast.success(`You have added ${selectRegion} to your watch list`);
+      }
+    }
+  };
+
+  // debug
   console.log(region);
+  console.log(selectRegion);
+  console.log(watchList);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="dashboard" element={<Dashboard region={region} />}>
+        <Route
+          path="dashboard"
+          element={
+            <Dashboard
+              region={region}
+              selectRegion={selectRegion}
+              handlerSelectOption={handlerSelectOption}
+              handlerAddWatchList={handlerAddWatchList}
+              ToastContainer={ToastContainer}
+              watchList={watchList}
+              isOptionSelected={isOptionSelected}
+            />
+          }
+        >
           <Route path="psi" element={<PsiNeaContainer />} />
           <Route path="pm25" element={<Pm25NeaContainer />} />
           <Route path="weather2hr" element={<Weather2HrsContainer />} />
           <Route path="wetbulb" element={<WetbulbTempController />} />
           <Route path="uv" element={<UvNeaContainer />} />
         </Route>
-        <Route path="watchlist" element={<WatchList />} />
-        <Route path="about" element={<About />} />
+        <Route path="watchlist" element={<WatchList watchList={watchList} />} />
+        <Route path="about" element={<About watchList={watchList} />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
@@ -43,3 +100,22 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // dependencies
 import { Outlet } from "react-router-dom";
 // components
@@ -11,17 +12,31 @@ import SelectionField from "../components/SelectionField";
 import SingaporeMap from "../components/SingaporeMap";
 
 function Dashboard(props) {
-  const { region } = props;
+  const {
+    region,
+    selectRegion,
+    handlerSelectOption,
+    handlerAddWatchList,
+    ToastContainer,
+    watchList,
+    isOptionSelected,
+  } = props;
   return (
     <>
-      <NavSideBar />
-
+      <NavSideBar watchList={watchList} />
       <div className="p-4 sm:ml-64">
+        <ToastContainer />
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
           <h1 className="text-center font-bold text-2xl mb-5">Dashboard</h1>
 
           <div className="mb-10 flex justify-center">
-            <SelectionField region={region} />
+            <SelectionField
+              region={region}
+              selectRegion={selectRegion}
+              handlerSelectOption={handlerSelectOption}
+              handlerAddWatchList={handlerAddWatchList}
+              isOptionSelected={isOptionSelected}
+            />
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-4">
             <PsiNea />
