7295642a0a9427f3686ed990b12462f0daa0e170
Justen Manni <me@justenmanni.dev>
2023-07-18 01:33:05 +0800

    Add weatherIcons data mapping

---
+++ src/data/weatherIcons.js
@@ -0,0 +1,61 @@
+const weatherIcons = [
+  { weather: "Fair", icon: "faCloud" },
+  {
+    weather: "Fair & Warm",
+    icon: "faCloudSun",
+  },
+  {
+    weather: "Partly Cloudy",
+    icon: "faCloud",
+  },
+  {
+    weather: "Cloudy",
+    icon: "faCloud",
+  },
+  {
+    weather: "Hazy",
+    icon: "faSmog",
+  },
+  {
+    weather: "Slightly Hazy",
+    icon: "faSmog",
+  },
+  {
+    weather: "Windy",
+    icon: "faWind",
+  },
+  {
+    weather: "Mist",
+    icon: "faSmog",
+  },
+  {
+    weather: "Light Rain",
+    icon: "faCloudRain",
+  },
+  {
+    weather: "Moderate Rain",
+    icon: "faCloudRain",
+  },
+  {
+    weather: "Heavy Rain",
+    icon: "faCloudShowersHeavy",
+  },
+  {
+    weather: "Passing Showers",
+    icon: "faCloudRain",
+  },
+  {
+    weather: "Showers",
+    icon: "faCloudRain",
+  },
+  {
+    weather: "Heavy Showers",
+    icon: "faCloudShowersHeavy",
+  },
+  {
+    weather: "Thundery Showers",
+    icon: "faCloudBolt",
+  },
+];
+
+export default weatherIcons;
