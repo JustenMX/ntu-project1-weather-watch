1e726348115b7b3d51822b3b90bf74c348445c0a
nelson6726e <cheefong0903@gmail.com>
2023-07-19 23:04:31 +0800

    added more text in read more

--- src/components/UvNea.jsx
+++ src/components/UvNea.jsx
@@ -1,8 +1,8 @@
 // dependencies
-import { useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Button from "./Button";
+import PropTypes from 'prop-types';
 
 function UvNea(props) {
 
@@ -11,15 +11,15 @@ function UvNea(props) {
   // Background color change
   const colourMatrixUvIndex = (uvIndex) => {
     if (uvIndex <= 2) {
-      return "bg-green-500";
+      return "bg-green-100";
     } else if (uvIndex <= 5) {
-      return "bg-yellow-500";
+      return "bg-yellow-200";
     } else if (uvIndex <= 7) {
-      return "bg-orange-500";
+      return "bg-orange-200";
     } else if (uvIndex <= 10) {
-      return "bg-red-500";     
+      return "bg-red-200";     
     } else {
-      return "bg-purple-500";
+      return "bg-purple-200";
     }
   };
 
@@ -27,26 +27,20 @@ function UvNea(props) {
 
   return (
     <>
-      <div className={`max-w-sm ${bgColor} border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700`}>
-        <a href="#">
-        <h1>Current UV Index</h1>
-        {uvIndex !== null ? (
-        <p>{uvIndex}</p>
-      ) : (
-        <p>Loading UV index...</p>
-      )}
+      <div className={`max-w-sm ${bgColor} border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex flex-col justify-center p-5`}>
+        <a href="#" className="my-auto">
+          <h5 className="text-4xl text-center font-bold mb-4">UV Index</h5>
+          {uvIndex !== null ? (
+            <p className="text-5xl text-center mb-4">{uvIndex}</p>
+          ) : (
+            <p className="text-4xl text-center mb-4">Loading UV index...</p>
+          )}
         </a>
-        <div className="p-5">
+        <div>
           <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              UV Index
-            </h5>
           </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          UV Index value averaged over the past hour. Updated every hour between 7 AM and 7 PM everyday.
-          </p>
           <Button
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-auto"
             buttonLabel={[
               "Read More",
               <FontAwesomeIcon
--- src/components/UvNeaContainer.jsx
+++ src/components/UvNeaContainer.jsx
@@ -36,6 +36,26 @@ function UvNeaContainer() {
               </tr>
             </tbody>
           </table>
+          <div className="w-full px-4 py-2 text-center text-gray-700 dark:text-white text-justify">
+          <p className="mb-4">The UVI is an international standard index that describes the level of solar UV radiation on the earth’s surface. The index ranges from 0 to 11+ and the values are grouped into various exposure categories. A higher index value indicates a greater potential for harmful effects to the skin and eyes.</p>
+            
+          <p className="mb-4">In Singapore, the UVI is measured at the Changi Meteorological Station and reported hourly between 7am and 7pm. The reported UVI represents its average value for the past 15 minutes. In the presence of rain and/or under cloudy conditions, the UVI value is reduced. Therefore, the UVI value may vary in other parts of the island.</p>
+
+          <p className="mb-4">Advisory on Sun Protection Measures *</p>
+
+          <p className="mb-4">Some ultraviolet (UV) exposure is important for health functions such as Vitamin D production in the body and treating diseases like jaundice in babies or psoriasis and rickets in adults.</p>
+
+          <p className="mb-4">However, excessive exposure to solar UV radiation can result in harmful effects to the skin and eyes. If you must be out in the sun, especially between 11am and 3pm when the UV index levels are highest, the following simple protective measures can help to minimise the effects.</p>
+
+            <ul className="list-disc list-inside mb-4">
+              <li>Use sunscreen (at least SPF 30)</li>
+              <li>Use an umbrella and seek shade</li>
+              <li>Wear sunglasses that block UVA/UVB rays</li>
+              <li>Wear a broad-brimmed hat</li>
+            </ul>
+
+            <p>* Source: Ministry of Health</p>
+          </div>
         </div>
       </div>
     </>
